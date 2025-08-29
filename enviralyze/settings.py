b816a1c61089fecbcb58d8b2fd01039063c31275
tykhoncode <tykhoncode@gmail.com>
2025-08-29 19:18:10 +0200

    🔐 chore(verifications): small update to email verification flow

--- enviralyze/settings.py
+++ enviralyze/settings.py
@@ -146,3 +146,12 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+BACKEND_URL = "http://127.0.0.1:8000"
